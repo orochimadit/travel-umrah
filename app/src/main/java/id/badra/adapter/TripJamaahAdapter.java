0ab8fa35fe529275b5e5c1656a064f4f2003a73f
Aditya Pratama <orochimadit@gmail.com>
2018-09-24 13:23:32 -0700

    trip jamaah

---
+++ app/src/main/java/id/badra/adapter/TripJamaahAdapter.java
@@ -0,0 +1,135 @@
+package id.badra.adapter;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import id.badra.model.TripHotel;
+import id.badra.model.TripJamaah;
+import id.badra.travelumrah.R;
+import id.badra.travelumrah.TripHotelFragmentDetail;
+
+public class TripJamaahAdapter extends RecyclerView.Adapter<TripJamaahAdapter.Viewholder>{
+    private ArrayList<TripJamaah> model;
+    private FragmentManager fms;
+
+    public TripJamaahAdapter(ArrayList<TripJamaah> model, FragmentManager fms) {
+        this.model = model;
+        this.fms = fms;
+    }
+
+    @NonNull
+    @Override
+    public TripJamaahAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.trip_hotel_item, parent, false);
+        view.setLayoutParams(new RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.WRAP_CONTENT));
+        return new TripJamaahAdapter.Viewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TripHotelAdapter.Viewholder holder, final int position) {
+        TripJamaah currentItem = model.get(position);
+
+        String id_trip              = currentItem.getMid_trip();
+        String id_trip_hotel        = currentItem.getMid_trip_hotel1();
+        String id_trip_la_paket     = currentItem.getMid_trip_la_paket();
+
+        String id_vendor            = currentItem.getMid_vendor();
+        String tanggal_booking      = currentItem.getMtanggal_booking();
+        String tanggal_checkin      = currentItem.getMtanggal_checkin();
+        String length_of_stay       = currentItem.getMlength_of_stay();
+        String tanggal_checkout     = currentItem.getMtanggal_checkout();
+        String jenis_kamar          = currentItem.getMjenis_kamar();
+        String opsi_kamar           = currentItem.getMopsi_kamar();
+        String harga_sewa           = currentItem.getMharga_sewa();
+        String jumlah_room          = currentItem.getMjumlah_room();
+        String view                 = currentItem.getMview();
+        String profit_margin        = currentItem.getMprofit_margin();
+        String rekening             = currentItem.getMrekening();
+        String harga_jual           = currentItem.getMharga_jual();
+        String booking_fee          = currentItem.getMbooking_fee();
+        String booking_fee_currency = currentItem.getMbooking_fee_currency();
+        String contact_mobile       = currentItem.getMcontact_mobile();
+        String review               = currentItem.getMreview();
+        String status               = currentItem.getMstatus();
+        String nama_hotel           = currentItem.getMnama_hotel();
+        String bintang              = currentItem.getMbintang();
+        String nama_vendor          = currentItem.getMnama_vendor();
+
+        holder.mTextViewNamaHotel.setText(nama_hotel);
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TripHotel obj = new TripHotel(model.get(position).getMid_trip_hotel1(),
+                        model.get(position).getMid_trip(),
+                        model.get(position).getMid_trip_la_paket(),
+                        model.get(position).getMid_hotel(),
+                        model.get(position).getMid_vendor(),
+                        model.get(position).getMtanggal_booking(),
+                        model.get(position).getMtanggal_checkin(),
+                        model.get(position).getMlength_of_stay(),
+                        model.get(position).getMtanggal_checkout(),
+                        model.get(position).getMjenis_kamar(),
+                        model.get(position).getMopsi_kamar(),
+                        model.get(position).getMharga_sewa(),
+                        model.get(position).getMjumlah_room(),
+                        model.get(position).getMview(),
+                        model.get(position).getMprofit_margin(),
+                        //id bank?
+                        model.get(position).getMrekening(),
+                        model.get(position).getMharga_jual(),
+                        model.get(position).getMbooking_fee(),
+                        model.get(position).getMbooking_fee_currency(),
+                        model.get(position).getMcontact_mobile(),
+                        model.get(position).getMreview(),
+                        model.get(position).getMstatus(),
+                        model.get(position).getMnama_hotel(),
+                        model.get(position).getMbintang(),
+                        model.get(position).getMnama_vendor(),
+                        model.get(position).getMcontact_email()
+                );
+                TripHotelFragmentDetail frgment = new TripHotelFragmentDetail();
+                Bundle args = new Bundle();
+                args.putParcelable("trip_hotel",obj);
+                frgment.setArguments(args);
+
+                fms.beginTransaction().replace(R.id.content_frames, frgment).commit();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return model.size();
+    }
+
+    public static class Viewholder extends RecyclerView.ViewHolder {
+
+        public TextView mTextViewNamaHotel;
+        public View mView;
+        private ImageView imageViewPoster;
+
+        public Viewholder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            mTextViewNamaHotel = itemView.findViewById(R.id.nama_hotel_item);
+
+        }
+
+//        public void setPosterPath(String posterPath) {
+//            //
+//            Picasso.with(mView.getContext()).load(StringsAPI.IMAGE_SOURCE+posterPath).placeholder(R.mipmap.ic_launcher)
+//                    .into(imageViewPoster);
+//        }
+    }
+}
--- app/src/main/java/id/badra/model/TripJamaah.java
+++ app/src/main/java/id/badra/model/TripJamaah.java
@@ -1,5 +1,370 @@
 package id.badra.model;
 
-public class TripJamaah {
+import android.os.Parcel;
+import android.os.Parcelable;
 
+public class TripJamaah implements Parcelable{
+    private String mid_trip_jamaah;
+    private String mid_trip;
+    private String mid_jamaah;
+    private String mid_paket_perjalanan;
+    private String mid_manasik;
+    private String mid_trip_visa;
+    private String mid_kk;
+    private String mrelasi;
+    private String mharga_jual;
+    private String mno_paspor;
+    private String mtgl_paspor;
+    private String mpaspor_isue;
+    private String mpaspor_expire_date;
+    private String mpaspor;
+    private String msurat_nikah;
+    private String mkartu_keluarga;
+    private String mbuku_kuning;
+    private String msurat_mahrom;
+    private String mfoto;
+    private String mbukti_tiket;
+    private String mtanggal_pengajuan;
+    private String mtanggal_mova;
+    private String mtanggal_tempel_sticker;
+    private String mnama_muasasah;
+    private String mid_vendor_visa;
+    private String mhotel_bintang_madinah;
+    private String mid_hotel_madinah;
+    private String mokupansi_madinah;
+    private String mkatering_madinah;
+    private String mhotel_bintang_makkah;
+    private String mid_hotel_makkah;
+    private String mokupansi_makkah;
+    private String mkatering_makkah;
+    private String mhotel_bintang_jeddah;
+    private String mid_hotel_jeddah;
+    private String mokupansi_jeddah;
+    private String mkatering_jeddah;
+    private String mroute_tambahan;
+    private String mnama_jamaah;
+    private String mtanggal_manasik;
+
+public TripJamaah(){
+}
+
+
+public TripJamaah(String id_trip_jamaah,String id_trip,String id_jamaah,String id_paket_perjalanan,String id_manasik,
+                  String id_trip_visa,String id_kk,String relasi,String harga_jual, String no_paspor,String tgl_paspor,
+                  String paspor_issuer,String paspor_expire_date,String paspor,String surat_nikah,String kartu_keluarga,
+                  String buku_kuning,String surat_mahrom,String foto,String bukti_tiket,String tanggal_pengajuan,String tanggal_mova,
+                  String tanggal_tempel_sticker,String nama_muasasah,
+                  String id_vendor_visa,String hotel_bintang_madinah,String id_hotel_madinah,
+                  String okupansi_madinah,String katering_madinah,
+                  String hotel_bintang_makkah,String id_hotel_makkah,String okupansi_makkah,String katering_makkah,
+                  String hotel_bintang_jeddah,String id_hotel_jeddah,String okupansi_jeddah,String katering_jeddah,
+                  String route_tambahan, String nama_jamaah,String tgl_manasik
+                  ){
+
+    mid_trip_jamaah         = id_trip_jamaah;
+    mid_trip                = id_trip;
+    mid_jamaah              = id_jamaah;
+    mid_paket_perjalanan    = id_paket_perjalanan;
+    mid_manasik             =id_manasik;
+    mid_trip_visa           =id_trip_visa;
+    mid_kk                  =id_kk;
+    mrelasi                 =relasi;
+    mharga_jual             =harga_jual;
+    mno_paspor              =no_paspor;
+    mtgl_paspor             =tgl_paspor;
+    mpaspor_isue            =paspor_issuer;
+    mpaspor_expire_date     =paspor_expire_date;
+    mpaspor                 =paspor;
+    msurat_nikah            =surat_nikah;
+    mkartu_keluarga         =kartu_keluarga;
+    mbuku_kuning            =buku_kuning;
+    msurat_mahrom           =surat_mahrom;
+    mfoto                   =foto;
+    mbukti_tiket            =bukti_tiket;
+    mtanggal_pengajuan      =tanggal_pengajuan;
+    mtanggal_mova           =tanggal_mova;
+    mtanggal_tempel_sticker =tanggal_tempel_sticker;
+    mnama_muasasah          =nama_muasasah;
+    mid_vendor_visa         =id_vendor_visa;
+    mhotel_bintang_madinah  =hotel_bintang_madinah;
+    mid_hotel_madinah       =hotel_bintang_madinah;
+    mokupansi_madinah       =okupansi_madinah;
+    mkatering_madinah       =katering_madinah;
+    mhotel_bintang_makkah   =hotel_bintang_makkah;
+    mid_hotel_makkah        =id_hotel_makkah;
+    mokupansi_makkah        =okupansi_makkah;
+    mkatering_makkah        =katering_makkah;
+    mid_hotel_jeddah        =id_hotel_jeddah;
+    mokupansi_jeddah        =okupansi_jeddah;
+    mkatering_jeddah        =katering_jeddah;
+    mnama_jamaah            =nama_jamaah;
+    mtanggal_manasik        =tgl_manasik;
+}
+
+
+    public String getMid_trip() {
+        return mid_trip;
+    }
+
+    public String getMid_jamaah() {
+        return mid_jamaah;
+    }
+
+    public String getMid_manasik() {
+        return mid_manasik;
+    }
+
+    public String getMid_paket_perjalanan() {
+        return mid_paket_perjalanan;
+    }
+
+    public String getMid_kk() {
+        return mid_kk;
+    }
+
+    public String getMid_trip_jamaah() {
+        return mid_trip_jamaah;
+    }
+
+    public String getMharga_jual() {
+        return mharga_jual;
+    }
+
+    public String getMid_trip_visa() {
+        return mid_trip_visa;
+    }
+
+    public String getMrelasi() {
+        return mrelasi;
+    }
+
+    public String getMfoto() {
+        return mfoto;
+    }
+
+    public String getMbukti_tiket() {
+        return mbukti_tiket;
+    }
+
+    public String getMbuku_kuning() {
+        return mbuku_kuning;
+    }
+
+    public String getMhotel_bintang_jeddah() {
+        return mhotel_bintang_jeddah;
+    }
+
+    public String getMhotel_bintang_madinah() {
+        return mhotel_bintang_madinah;
+    }
+
+    public String getMhotel_bintang_makkah() {
+        return mhotel_bintang_makkah;
+    }
+
+    public String getMid_hotel_jeddah() {
+        return mid_hotel_jeddah;
+    }
+
+    public String getMid_hotel_madinah() {
+        return mid_hotel_madinah;
+    }
+
+    public String getMid_hotel_makkah() {
+        return mid_hotel_makkah;
+    }
+
+    public String getMid_vendor_visa() {
+        return mid_vendor_visa;
+    }
+
+    public String getMkartu_keluarga() {
+        return mkartu_keluarga;
+    }
+
+    public String getMkatering_madinah() {
+        return mkatering_madinah;
+    }
+
+    public String getMkatering_jeddah() {
+        return mkatering_jeddah;
+    }
+
+    public String getMkatering_makkah() {
+        return mkatering_makkah;
+    }
+
+    public String getMnama_muasasah() {
+        return mnama_muasasah;
+    }
+
+    public String getMno_paspor() {
+        return mno_paspor;
+    }
+
+    public String getMnama_jamaah() {
+        return mnama_jamaah;
+    }
+
+    public String getMokupansi_jeddah() {
+        return mokupansi_jeddah;
+    }
+
+    public String getMokupansi_madinah() {
+        return mokupansi_madinah;
+    }
+
+    public String getMokupansi_makkah() {
+        return mokupansi_makkah;
+    }
+
+    public String getMpaspor() {
+        return mpaspor;
+    }
+
+    public String getMpaspor_expire_date() {
+        return mpaspor_expire_date;
+    }
+
+    public String getMpaspor_isue() {
+        return mpaspor_isue;
+    }
+
+    public String getMsurat_mahrom() {
+        return msurat_mahrom;
+    }
+
+    public String getMsurat_nikah() {
+        return msurat_nikah;
+    }
+
+    public String getMtgl_paspor() {
+        return mtgl_paspor;
+    }
+
+    public String getMroute_tambahan() {
+        return mroute_tambahan;
+    }
+
+    public String getMtanggal_manasik() {
+        return mtanggal_manasik;
+    }
+
+    public String getMtanggal_mova() {
+        return mtanggal_mova;
+    }
+
+    public String getMtanggal_pengajuan() {
+        return mtanggal_pengajuan;
+    }
+
+    public String getMtanggal_tempel_sticker() {
+        return mtanggal_tempel_sticker;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.mid_trip_jamaah);
+        dest.writeString(this.mid_trip);
+        dest.writeString(this.mid_jamaah);
+        dest.writeString(this.mid_paket_perjalanan);
+        dest.writeString(this.mid_manasik);
+        dest.writeString(this.mid_trip_visa);
+        dest.writeString(this.mid_kk);
+        dest.writeString(this.mrelasi);
+        dest.writeString(this.mharga_jual);
+        dest.writeString(this.mno_paspor);
+        dest.writeString(this.mtgl_paspor);
+        dest.writeString(this.mpaspor_isue);
+        dest.writeString(this.mpaspor_expire_date);
+        dest.writeString(this.mpaspor);
+        dest.writeString(this.msurat_nikah);
+        dest.writeString(this.mkartu_keluarga);
+        dest.writeString(this.mbuku_kuning);
+        dest.writeString(this.msurat_mahrom);
+        dest.writeString(this.mfoto);
+        dest.writeString(this.mbukti_tiket);
+        dest.writeString(this.mtanggal_pengajuan);
+        dest.writeString(this.mtanggal_mova);
+        dest.writeString(this.mtanggal_tempel_sticker);
+        dest.writeString(this.mnama_muasasah);
+        dest.writeString(this.mid_vendor_visa);
+        dest.writeString(this.mhotel_bintang_madinah);
+        dest.writeString(this.mid_hotel_madinah);
+        dest.writeString(this.mokupansi_madinah);
+        dest.writeString(this.mkatering_madinah);
+        dest.writeString(this.mhotel_bintang_makkah);
+        dest.writeString(this.mid_hotel_makkah);
+        dest.writeString(this.mokupansi_makkah);
+        dest.writeString(this.mkatering_makkah);
+        dest.writeString(this.mhotel_bintang_jeddah);
+        dest.writeString(this.mid_hotel_jeddah);
+        dest.writeString(this.mokupansi_jeddah);
+        dest.writeString(this.mkatering_jeddah);
+        dest.writeString(this.mroute_tambahan);
+        dest.writeString(this.mnama_jamaah);
+        dest.writeString(this.mtanggal_manasik);
+    }
+
+    protected TripJamaah(Parcel in) {
+        this.mid_trip_jamaah = in.readString();
+        this.mid_trip = in.readString();
+        this.mid_jamaah = in.readString();
+        this.mid_paket_perjalanan = in.readString();
+        this.mid_manasik=in.readString();
+        this.mid_trip_visa=in.readString();
+        this.mid_kk=in.readString();
+        this.mrelasi=in.readString();
+        this.mharga_jual=in.readString();
+        this.mno_paspor=in.readString();
+        this.mtgl_paspor=in.readString();
+        this.mpaspor_isue=in.readString();
+        this.mpaspor_expire_date=in.readString();
+        this.mpaspor      =in.readString();
+        this.msurat_nikah=in.readString();
+        this.mkartu_keluarga=in.readString();
+        this.mbuku_kuning=in.readString();
+        this.msurat_mahrom=in.readString();
+        this.mfoto=in.readString();
+        this.mbukti_tiket=in.readString();
+        this.mtanggal_pengajuan=in.readString();
+        this.mtanggal_mova=in.readString();
+        this.mtanggal_tempel_sticker=in.readString();
+        this.mnama_muasasah=in.readString();
+        this.mid_vendor_visa=in.readString();
+        this.mhotel_bintang_madinah=in.readString();
+        this.mid_hotel_madinah=in.readString();
+        this.mokupansi_madinah=in.readString();
+        this.mkatering_madinah=in.readString();
+        this.mhotel_bintang_makkah=in.readString();
+        this.mid_hotel_makkah=in.readString();
+        this.mokupansi_makkah=in.readString();
+        this.mkatering_makkah=in.readString();
+        this.mhotel_bintang_jeddah=in.readString();
+        this.mid_hotel_jeddah=in.readString();
+        this.mokupansi_jeddah=in.readString();
+        this.mkatering_jeddah=in.readString();
+        this.mroute_tambahan=in.readString();
+        this.mnama_jamaah=in.readString();
+        this.mtanggal_manasik=in.readString();
+
+    }
+
+    public static final Parcelable.Creator<TripJamaah> CREATOR = new Parcelable.Creator<TripJamaah>() {
+
+        @Override
+        public TripJamaah createFromParcel(Parcel source) {
+            return new TripJamaah(source);
+        }
+
+        @Override
+        public TripJamaah[] newArray(int size) {
+            return new TripJamaah[size];
+        }
+    };
 }
